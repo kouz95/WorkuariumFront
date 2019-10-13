84e6eae2afbf44d0514ecbef8348b2d650871f2e
kouz <lastpuzly@gmail.com>
2019-10-13 18:51:10 +0900

    localStorage에 있는 userName get, console.log

---
+++ chat/image.js
@@ -0,0 +1,21 @@
+chrome.storage.sync.get(['name'], function(result) {
+    console.log('Value currently is ' + result.name);
+  });
+
+const imageContainer = document.querySelector(".js-image"),
+    imageButton = imageContainer.querySelector("button");
+
+function popupImage(){
+    whale.windows.create({
+        url: whale.extension.getURL("../canvas/index.html"), type: "popup", left: 10, top: 150, width: 1280, height: 970 
+    })
+}
+
+function clickCheck(){
+    imageButton.addEventListener('click', popupImage ,false)
+}
+
+function init(){
+    clickCheck();
+}
+init();
