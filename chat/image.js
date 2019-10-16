9ae9625e3cb4bbd24a19b8530e7ce734b04c6cfd
KNY <kny011598@gmail.com>
2019-10-16 21:40:54 +0900

    workspace size

--- chat/image.js
+++ chat/image.js
@@ -5,9 +5,20 @@ chrome.storage.sync.get(['name'], function(result) {
 const imageContainer = document.querySelector(".js-image"),
     imageButton = imageContainer.querySelector("button");
 
+const windowWidth = window.screen.width,
+    windowHeight = window.screen.height,
+    sidebarWidth = document.documentElement.clientWidth;
+
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
 function popupImage(){
     whale.windows.create({
-        url: whale.extension.getURL("../testCanvas/index.html"), type: "popup", left: 10, top: 150, width: 1280, height: 970 
+        url: whale.extension.getURL("../testCanvas/index.html"),
+        type: "popup",
+        left: 10,
+        top: 100,
+        width: Math.round((windowWidth - sidebarWidth) * 0.9),
+        height: Math.round(windowHeight * 0.9) 
     })
 }
 
